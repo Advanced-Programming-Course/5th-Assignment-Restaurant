955e85be483a39e3cc8a8a87b49d00d206d427ec
Ashkan Asef <ash.seah.1@gmail.com>
2024-05-03 04:59:36 -0700

    added Invoice class

--- first/restaurant/Item.java
+++ first/restaurant/Item.java
@@ -3,7 +3,7 @@ public class Item {
     private final Food food;
     private final int count;
     private final String description;
-    
+
     public Item(Food food, int count, String description) {
         this.food = food;
         this.count = count;
