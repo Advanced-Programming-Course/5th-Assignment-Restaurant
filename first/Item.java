069942655e3edb85ab910a44a8c7c44aa82d7fa7
mohadesemoradzade <mohadesemoradzade3@gmail.com>
2024-05-04 17:25:04 +0330

    create & change invoice, item  filse

--- first/Item.java
+++ first/Item.java
@@ -1,5 +1,25 @@
 package first;
-
 public class Item {
-    
+    private final Food food;
+    private final int count;
+    private final String description;
+
+    public Item(Food food, int count, String description) {
+        this.food = food;
+        this.count = count;
+        if(description != null) {
+            this.description = description;
+        } else {
+            this.description = "";
+        }
+    }
+    public Food getFood() {
+        return food;
+    }
+    public int getCount() {
+        return count;
+    }
+    public String getDescription() {
+        return description;
+    }
 }
