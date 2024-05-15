04b9de477fcaac7ea48c315dc92d7786396f3a69
MelikaShirvani <melikashiravani001@gmail.com>
2024-05-15 15:51:45 +0300

    item Obj

---
+++ restaurant/Item.java
@@ -0,0 +1,29 @@
+package restaurant;
+
+public class Item {
+    private final Food food;
+    private final int count;
+    private final String description;
+
+    public Item(Food food, int count) {
+        this(food, count, "");
+    }
+
+    public Item(Food food, int count, String description) {
+        this.food = food;
+        this.count = count;
+        this.description = description;
+    }
+
+    public Food getFood() {
+        return food;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
